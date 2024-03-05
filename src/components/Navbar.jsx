97838bfd99f9d54530414891e694b7efd8d1ea73
Andrew Cookson <andrewcookson@email.com>
2024-03-05 20:58:53 +0000

    enhancement: FE1 created logged in navbar state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import "../styles/Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { UserRound } from "lucide-react";
 
 export default function Navbar() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <header>
       <nav>
         <div className="nav-left">
-          <div className="brand-logo">Swaps</div>
+          <Link className="brand-logo" to="/">
+            Swaps
+          </Link>
           <div className="nav-links-left">
             <Link to="/">Home</Link>
             <Link to="/about">Account</Link>
@@ -14,8 +19,21 @@ export default function Navbar() {
         </div>
         <div className="nav-right">
           <div className="nav-links-right">
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
+            {loggedIn ? (
+              <>
+                <Link to="/account">
+                  <UserRound />
+                </Link>
+                <button>Logout</button>
+              </>
+            ) : (
+              <>
+                <Link to="/register">Register</Link>
+                <Link className="login-link" to="/login">
+                  Login
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </nav>
