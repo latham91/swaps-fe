209ee113a13ac6c935c2c502598cf3d0f4a4b190
Andyyyy987 <am38387@gmail.com>
2024-03-06 16:47:56 +0000

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import ProductPage from "./pages/ProductPage";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
