180147f15494da79f893c0a6c1c5513167743541
Aaron Latham <latham91@icloud.com>
2024-03-06 14:56:47 +0000

    enhancement: FE6 - create sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
+import SignUpPage from "./pages/SignUpPage";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </>
   );
