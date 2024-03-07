e79f8f68567e5b0e01b8cd37a070ed411b2c8615
Aaron Latham <latham91@icloud.com>
2024-03-07 10:25:24 +0000

    enhancement: create and setup auth context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import AuthProvider from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
