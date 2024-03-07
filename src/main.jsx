fb59b29bb9db8865523ba3b85fc6c7a25ef432e4
Aaron Latham <latham91@icloud.com>
2024-03-07 10:32:17 +0000

    chore: updated import in main.jsx for auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
