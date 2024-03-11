2aa3ef9c23a9ef0441aa79dc5ad51bd88a7a5759
Aaron Latham <latham91@icloud.com>
2024-03-11 13:01:48 +0000

    chore: fixed h1 in pagenotfound

--- src/pages/PageNotFoundPage.jsx
+++ src/pages/PageNotFoundPage.jsx
@@ -1,12 +1,12 @@
 import "../styles/PageNotFoundPage.css";
 
 function PageNotFoundPage() {
-    return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist</p>
-        </div>
-    );    
-};
+  return (
+    <div className="pagenotfound-container">
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist</p>
+    </div>
+  );
+}
 
 export default PageNotFoundPage;
